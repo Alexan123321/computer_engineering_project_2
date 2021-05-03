1f5ef7fa84429d15a2bd79ea1f11b1299a5e7517
Alexan123321 <60431697+Alexan123321@users.noreply.github.com>
2021-05-03 20:08:02 +0200

    Add files via upload
    Added model, controller logic and threaded alarm functionality. 
    Needs cleanup, commenting, correct logic implementation and threading.

--- kitchen_guard_controller.py
+++ kitchen_guard_controller.py
@@ -1,55 +1,60 @@
 from kitchen_guard_mqtt_client import kitchenGuardMqttClient, z2mMsg    #importing the kitchenGuardMQTTClient and the z2mMsg declared in the kitchen_guard_mqtt_client.py file
+from kitchen_guard_model import kitchenGuardModel, zigbeeDevice
 from enum import Enum                                                   #importing enumerated types
+import time
+import threading
+
+# TODO: Needs commenting and CLEANUP
+# TODO: Needs correct implementation of controller logic on _start_alarm function
+# TODO: Needs correct thread implementation - alarmThreadState cannot be accessed from the start alarm function?
 
 # Enumerated state type #
 class State(Enum):
     ON = "ON"
     OFF = "OFF"
 
-# TODO: friendlyName logic must be transferred to instantiations in main and objects in the model
-# Friendly name declarations #
-PIR = "0x00158d0005727f3a"
-PLUG = "0xccccccfffee3d7aa"
-LED = "0xbc33acfffe8b8ea8"
-
 class kitchenGuardController:
     kitchenGuardState:State
-    occupancyState:State
-    stoveState:State
-    LEDState:State
+    ctlStoveState:State
+    alarmThreadState:int
     port:str
     host:str
 
-    def __init__(self):
-        pass
+    devicesModel:kitchenGuardModel
+
+    def __init__(self, inputKitchenGuardModel:kitchenGuardModel):
+        self.devicesModel = inputKitchenGuardModel
+        self.alarmThread = threading.Thread(target = self._startAlarm)
+        self.alarmThread.start()
     
-    # TODO: Controller-logik skal implementeres her 
     #when publish message is received from the device, then this function is run
     #def on_message(client, userdata, msg):
-    def ctl_on_message(self, msg : z2mMsg):        
-        #if the topic is published by the PIR sensor
-        if msg.topic == "zigbee2mqtt/" + PIR:
-            self.occupancyState = msg.payload["occupancy"]
-            if msg.payload["occupancy"] == True:
-                self.myClient.publish_msg("zigbee2mqtt/" + LED + "/set/state", "OFF")
-            else:
-                self.myClient.publish_msg("zigbee2mqtt/" + LED + "/set/state", "ON")
-        
-        #if the topic is published by the power plug
-        elif msg.topic == "zigbee2mqtt/" + PLUG:
-            self.stoveState = msg.payload["state"]
-        
-        #if the topic is published by the LED
-        else:
-            self.LEDState = msg.payload["state"]
+    def ctl_on_message(self, msg:z2mMsg):
+        #Kitchen is empty and stove is on
+        currDevice = self.devicesModel.findDevice(msg.topic)
+        print("Controller event received!")
+        #Occupancy status skal lige byttes rundt nedenstående
+        if((currDevice.getType() == "pir") and (currDevice.getLocation() == "kitchen") and (msg.payload["occupancy"] == True) and (self.ctlStoveState == "ON") and (self.alarmThreadState == 0)):
+            print("Beder om at starte alarm!")
+            self.alarmThreadState = 1
+
+        #Kitchen is no longer empty, but stove is on
+        elif((currDevice.getType() == "pir") and (currDevice.getLocation() == "kitchen") and (msg.payload["occupancy"] == False) and (self.ctlStoveState == "ON") and (self.alarmThreadState == 1)): #Missing:  and (self.alarmThreadState == "ON")
+            print("Beder alarm om at stoppe")
+            alarmThreadState = 0
+
+        if(currDevice.getType() == "plug"):
+            print("Stove state updated!")
+            self.ctlStoveState = msg.payload["state"]
+            print(self.ctlStoveState)
 
     def start(self, inputPort:int, inputHost:str):
         self.kitchenGuardState = State.ON
-        self.occupancyState = State.OFF
-        self.stoveState : State.OFF
+        self.ctlStoveState = State.OFF
+        self.alarmThreadState = 0
         self.port = inputPort
         self.host = inputHost
-        self.myClient = kitchenGuardMqttClient(self.host, self.port, on_message_clbk = self.ctl_on_message)
+        self.myClient = kitchenGuardMqttClient(self.host, self.port, self.devicesModel, on_message_clbk = self.ctl_on_message)
         self.myClient.start()
         pass
 
@@ -58,23 +63,45 @@ class kitchenGuardController:
         self.myClient.stop()
         pass
 
-    # TODO: Skal implementeres - her ligger "timerlogikken"
     def _startAlarm(self):
-        pass
+        #preemptive alarm
+        timerLimit = 18 * 1 #change *1 to *60 to get minutes
+        currentTime = int(time.time())
+        while(self.alarmThreadState == 1):
+            time.sleep(1)
+            if(int(time.time() >= currentTime + timerLimit)):
+                break
+        print("Tænder lys!")
+        self._turnOnLED()
 
-    # TODO: Skal implementeres
+        #postemptive alarm
+        timerLimit = 2 * 1 #change *1 to *60 to get minutes
+        currentTime = int(time.time())
+        while(self.alarmThreadState == 1):
+            time.sleep(1)
+            if(int(time.time() >= currentTime + timerLimit)):
+                break
+        print("Stopper alarm")      
+        self.stopAlarm()
+
     def stopAlarm(self):
+        self._turnOffStove()
         self._turnOffLED()
-        pass
+        self.alarmThreadState = 0
+        self.alarmThread.join()
 
-     # TODO: Skal implementeres
     def _turnOffStove(self):
-        self.myClient.publish_msg("zigbee2mqtt/" + PLUG + "/set/state", "OFF")
+        plugList = self.devicesModel.getDevices("plug")
+        for currDevice in plugList:
+            self.myClient.publish_msg("zigbee2mqtt/" + currDevice.getFriendlyName() + "/set/state", "OFF")
 
-    #TODO: When model is implemented, this function must turn ALL LEDs on
     def _turnOnLED(self):
-        self.myClient.publish_msg("zigbee2mqtt/" + LED + "/set/state", "ON")
+        ledList = self.devicesModel.getDevices("led")                           #a sublist of all pir sensors is computed then computed
+        for currDevice in ledList:                                              #finally, a sublist of all leds is computed
+            self.myClient.publish_msg("zigbee2mqtt/" + currDevice.getFriendlyName() + "/set/state", "ON") #whereby the client can unsubscribe to all the LEDs in the model
 
-    #TODO: When model is implemented, this function must turn ALL LEDs off
     def _turnOffLED(self):
-        self.myClient.publish_msg("zigbee2mqtt/" + LED + "/set/state", "OFF")
+        ledList = self.devicesModel.getDevices("led")                           #a sublist of all pir sensors is computed then computed
+        for currDevice in ledList:                                              #finally, a sublist of all leds is computed
+            self.myClient.publish_msg("zigbee2mqtt/" + currDevice.getFriendlyName() + "/set/state", "OFF") #whereby the client can unsubscribe to all the LEDs in the model
+
