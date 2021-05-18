58d1889858fdae0ac32966593d699fecebfb41c4
Alexan123321 <60431697+Alexan123321@users.noreply.github.com>
2021-05-18 19:02:37 +0200

    Add files via upload

---
+++ windows/application/kitchen_guard_master_main.py
@@ -0,0 +1,34 @@
+from time import sleep    
+
+from kitchen_guard_master_controller import KitchenGuardMasterController, State
+from kitchen_guard_master_model import KitchenGuardMasterModel
+
+dbHost = "localhost"            
+#database host is on localhost
+dbName = "kg_data"              
+#database name
+dbUser = "root"                 
+#username
+dbPass = "root"                 
+#password
+
+mqttHost = "192.168.87.104"         
+#connect to pi ip address
+mqttPort = 1883                     
+#using TCP (MQTT) port 1883 to communicate over                  
+
+def main():
+    model = KitchenGuardMasterModel(dbHost, dbName, dbUser, dbPass)
+    model.start()                                   
+    #start connecting to database
+
+    controller = KitchenGuardMasterController(model)
+    controller.start(mqttPort, mqttHost)            
+    #start controller where we listen for pi to publish events
+    while controller.kitchen_guard_state == State.ON:
+        sleep(1)                                    
+        #sleep for 1 second while the kitchen_guard_state is on
+
+
+if __name__ == "__main__":
+    main()
